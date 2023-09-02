d291d3ab80a4390dfe3531755c4a2f6c8e927394
asticash <142632941+asticash@users.noreply.github.com>
2023-09-02 16:17:24 -0300

    Add files via upload

---
+++ web/enviar-mail.php
@@ -0,0 +1,28 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(
+        !empty($_POST['nombre'])
+        && !empty($_POST['empresa'])
+        && !empty($_POST['telefono'])
+        && !empty($_POST['email'])
+        && !empty($_POST['mensaje'])
+    ){
+        $nombre = $_POST["nombre"];
+        $telefono = $_POST["empresa"];
+        $telefono = $_POST["telefono"];
+        $email = $_POST["email"];
+        $mensaje = $_POST["mensaje"];
+
+
+        $to = "mcash91@gmail.com";
+        $subject = "Nuevo mensaje";
+        $body = "Nombre: {$nombre}\n Empresa: {$empresa}\nEmail: {$email}\nTelefono: {$telefono}\nMensaje: {$mensaje}";
+        $headers = "De: {$email}";
+
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Mensaje enviado correctamente!";
+        } else {
+            echo "Error al enviar el mensaje.";
+        }
+    }
+}
